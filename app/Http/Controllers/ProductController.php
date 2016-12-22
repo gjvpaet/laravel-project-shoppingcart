46f242f98fecec04871ec05b5f058391757f8435
Gabriel Joshua Paet <gjvpaet@gmail.com>
2016-12-22 15:39:35 +0800

    Added add to cart functionality

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,11 @@
 
 namespace ShoppingCart\Http\Controllers;
 
-use ShoppingCart\Product;
 use Illuminate\Http\Request;
 
+use Session;
+use ShoppingCart\Cart;
+use ShoppingCart\Product;
 use ShoppingCart\Http\Requests;
 
 class ProductController extends Controller
@@ -14,5 +16,30 @@ class ProductController extends Controller
         $products = Product::all();
 
         return view('shop.index', ['products' => $products]);
+    }
+
+    public function getAddToCart(Request $request, $id)
+    {
+        $product = Product::find($id);
+
+        $oldCart = $request->session()->has('cart') ? $request->session()->get('cart') : null;
+        $cart = new Cart($oldCart);
+        $cart->add($product, $product->id);
+
+        $request->session()->put('cart', $cart);
+
+        return redirect()->route('product.index');
+    }
+
+    public function getCart(Request $request)
+    {
+        if (!$request->session()->has('cart')) {
+            return view('shop.shoppingCart');
+        }
+
+        $oldCart = $request->session()->get('cart');
+        $cart = new Cart($oldCart);
+
+        return view('shop.shoppingCart', ['products' => $cart->items, 'totalPrice' => $cart->totalPrice]);
     }
 }
